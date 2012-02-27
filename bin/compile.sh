cc27175cc7609a78b2677de346385649f6a6301b
Travis Desell <travis.desell@gmail.com>
2012-02-27 15:55:49 -0600

    Updated the compile.sh script

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,4 +1,5 @@
 cd ../client/
-g++ -Wall -DVERBOSE -DHTML_OUTPUT -DENABLE_CHECKPOINTING -DENABLE_COLOR -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
+g++ -Wall -DVERBOSE -DENABLE_CHECKPOINTING -DENABLE_COLOR -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
+#g++ -Wall -DVERBOSE -DHTML_OUTPUT -DENABLE_CHECKPOINTING -DENABLE_COLOR -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
 #g++ -Wall -DVERBOSE -DENABLE_CHECKPOINTING -DENABLE_COLOR -DFALSE_ONLY -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
 cd ../bin/
