2a65cc860f61fff80684454af3758bba016057b6
Travis Desell <travis.desell@gmail.com>
2012-04-16 17:02:34 -0500

    Added the n_choose_k header file.

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,8 +1,8 @@
 cd ../client/
 #g++ -Wall -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
 #g++ -Wall -DVERBOSE -DENABLE_COLOR -DSHOW_SUM_CALCULATION-DSHOW_SUM_CALCULATION  -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
-g++ -DVERBOSE -DENABLE_COLOR -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
+#g++ -DVERBOSE -DENABLE_COLOR -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
 #g++ -Wall -DVERBOSE -DENABLE_CHECKPOINTING -DENABLE_COLOR -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
-#g++ -Wall -DVERBOSE -DHTML_OUTPUT -DENABLE_CHECKPOINTING -DENABLE_COLOR -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
+g++ -Wall -DVERBOSE -DHTML_OUTPUT -DENABLE_COLOR -DFALSE_ONLY -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
 #g++ -Wall -DVERBOSE -DENABLE_CHECKPOINTING -DENABLE_COLOR -DFALSE_ONLY -O3 -msse3 -funroll-loops -ftree-vectorize subset_sum_main.cpp -o ../bin/subset_sum
 cd ../bin/
--- server/sss_work_generator.cpp
+++ server/sss_work_generator.cpp
@@ -44,6 +44,8 @@
 #include "sched_msgs.h"
 #include "str_util.h"
 
+#include "../common/n_choose_k.hpp"
+
 #define CUSHION 10
     // maintain at least this many unsent results
 #define REPLICATION_FACTOR  1
@@ -59,15 +61,17 @@ int seqno;
 
 // create one new job
 //
-int make_job() {
+int make_job(unsigned int max_set_value, unsigned int set_size, unsigned long long starting_set, unsigned long long sets_to_evaluate) {
     DB_WORKUNIT wu;
     char name[256], path[256];
+    char command_line[512];
     const char* infiles[1];
     int retval;
 
     // make a unique name (for the job and its input file)
     //
-    sprintf(name, "%s_%d_%d", app_name, start_time, seqno++);
+    sprintf(name, "%s_%u_%u_%llu", app_name, max_set_value, set_size, starting_set);
+    fprintf(stdout, "name: '%s'\n", name);
 
     // Create the input file.
     // Put it at the right place in the download dir hierarchy
@@ -79,13 +83,18 @@ int make_job() {
     fprintf(f, "This is the input file for job %s", name);
     fclose(f);
 
+    double number_of_sets = 1e9;        //TODO: figure out the number of sets for a decent sized workunit
+    double fpops_per_set = 1e3;         //TODO: figure out an estimate of how many fpops per set calculation
+    double fpops_est = fpops_per_set * number_of_sets;
+
+
     // Fill in the job parameters
     //
     wu.clear();
     wu.appid = app.id;
     strcpy(wu.name, name);
-    wu.rsc_fpops_est = 1e12;
-    wu.rsc_fpops_bound = 1e14;
+    wu.rsc_fpops_est = fpops_est;
+    wu.rsc_fpops_bound = fpops_est * 100;
     wu.rsc_memory_bound = 1e8;
     wu.rsc_disk_bound = 1e8;
     wu.delay_bound = 86400;
@@ -94,11 +103,16 @@ int make_job() {
     wu.max_error_results = REPLICATION_FACTOR*4;
     wu.max_total_results = REPLICATION_FACTOR*8;
     wu.max_success_results = REPLICATION_FACTOR*4;
-    infiles[0] = name;
+//    infiles[0] = name;
 
     // Register the job with BOINC
     //
     sprintf(path, "templates/%s", out_template_file);
+
+    sprintf(command_line, " %u %u %llu %llu", max_set_value, set_size, starting_set, sets_to_evaluate);
+    fprintf(stdout, "command line: '%s'\n", command_line);
+
+    /*
     return create_work(
         wu,
         in_template,
@@ -106,45 +120,52 @@ int make_job() {
         config.project_path(path),
         infiles,
         1,
-        config
+        config,
+        command_line
     );
+    */
+    return 1;
 }
 
-void main_loop() {
+int make_jobs(unsigned int max_set_value, unsigned int set_size) {
+    int unsent_results;
     int retval;
 
-    while (1) {
-        check_stop_daemons();
-        int n;
-        retval = count_unsent_results(n, 0);
-        if (retval) {
-            log_messages.printf(MSG_CRITICAL,
-                "count_unsent_jobs() failed: %s\n", boincerror(retval)
-            );
-            exit(retval);
-        }
-        if (n > CUSHION) {
-            sleep(10);
-        } else {
-            int njobs = (CUSHION-n)/REPLICATION_FACTOR;
-            log_messages.printf(MSG_DEBUG,
-                "Making %d jobs\n", njobs
-            );
-            for (int i=0; i<njobs; i++) {
-                retval = make_job();
-                if (retval) {
-                    log_messages.printf(MSG_CRITICAL,
-                        "can't make job: %s\n", boincerror(retval)
-                    );
-                    exit(retval);
-                }
-            }
-            // Now sleep for a few seconds to let the transitioner
-            // create instances for the jobs we just created.
-            // Otherwise we could end up creating an excess of jobs.
-            sleep(5);
-        }
+    check_stop_daemons();   //This checks to see if there is a stop in place, if there is it will exit the work generator.
+
+	//Aaron Comment: retval tells us if the count_unsent_results
+	//function is working properly. If it is, then it's value
+	//should be 0. Anything creater than 0 and the program exits.
+    retval = count_unsent_results(unsent_results, 0);
+    if (retval) {
+        log_messages.printf(MSG_CRITICAL,
+            "count_unsent_jobs() failed: %s\n", boincerror(retval)
+        );
+        exit(retval);
     }
+
+    //divide up the sets into mostly equal sized workunits
+
+    unsigned int SETS_PER_WORKUNIT = 2203961430;    //maybe have this as a #define
+                                                    //this should give a workunit around 30 minutes
+
+    unsigned long long total_sets = n_choose_k(max_set_value, set_size);
+    unsigned long long current_set = 0;
+
+    unsigned long long total_generated = 0;
+
+    while (current_set < total_sets) {
+        if ((total_sets - current_set) > SETS_PER_WORKUNIT) {
+            make_job(max_set_value, set_size, current_set, SETS_PER_WORKUNIT);
+        } else {
+            make_job(max_set_value, set_size, current_set, total_sets - current_set);
+        }
+        current_set += SETS_PER_WORKUNIT;
+
+        total_generated++;
+    }
+
+    fprintf(stdout, "workunits generated: %llu\n", total_generated);
 }
 
 void usage(char *name) {
@@ -159,12 +180,14 @@ void usage(char *name) {
         "  and sequence number, so that they're unique.\n\n"
         "Usage: %s [OPTION]...\n\n"
         "Options:\n"
-        "  [ --app X                Application name (default: example_app)\n"
-        "  [ --in_template_file     Input template (default: example_app_in)\n"
-        "  [ --out_template_file    Output template (default: example_app_out)\n"
-        "  [ -d X ]                 Sets debug level to X.\n"
-        "  [ -h | --help ]          Shows this help text.\n"
-        "  [ -v | --version ]       Shows version information.\n",
+        "  --app X                      Application name (default: example_app)\n"
+        "  --in_template_file           Input template (default: example_app_in)\n"
+        "  --out_template_file          Output template (default: example_app_out)\n"
+        "  -m | --max_set_value         The maximum value in the sets to be generated (REQUIRED).\n"       //Added for the subset sum problem
+        "  -n | --set_size              The size of the sets to be generated (REQUIRED).\n",               //Added for the subset sum problem
+        "  [ -d X ]                     Sets debug level to X.\n"
+        "  [ -h | --help ]              Shows this help text.\n"
+        "  [ -v | --version ]           Shows version information.\n",
         name
     );
 }
@@ -172,7 +195,12 @@ void usage(char *name) {
 int main(int argc, char** argv) {
     int i, retval;
     char buf[256];
+    unsigned int max_set_value, set_size;
 
+    bool max_set_value_found = false;
+    bool set_size_found = false;
+
+//Aaron Comment: command line flags are explained in descriptions above.
     for (i=1; i<argc; i++) {
         if (is_arg(argv[i], "d")) {
             if (!argv[++i]) {
@@ -195,6 +223,15 @@ int main(int argc, char** argv) {
         } else if (is_arg(argv[i], "v") || is_arg(argv[i], "version")) {
             printf("%s\n", SVN_VERSION);
             exit(0);
+
+        } else if (is_arg(argv[i], "m") || is_arg(argv[i], "max_set_value")) {
+            max_set_value = atoi(argv[++i]);
+            max_set_value_found = true;
+
+        } else if (is_arg(argv[i], "n") || is_arg(argv[i], "set_size")) {
+            set_size = atoi(argv[++i]);
+            set_size_found = true;
+
         } else {
             log_messages.printf(MSG_CRITICAL, "unknown command line argument: %s\n\n", argv[i]);
             usage(argv[0]);
@@ -202,6 +239,15 @@ int main(int argc, char** argv) {
         }
     }
 
+    if ( !(max_set_value_found && set_size_found) ) {
+        usage(argv[0]);
+        exit(0);
+    }
+
+//Aaron Comment: if at any time the retval value is greater than 0, then the program
+//has failed in some manner, and the program then exits.
+
+//Aaron Comment: processing project's config file.
     retval = config.parse_file();
     if (retval) {
         log_messages.printf(MSG_CRITICAL,
@@ -210,6 +256,7 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
+//Aaron Comment: opening connection to database.
     retval = boinc_db.open(
         config.db_name, config.db_host, config.db_user, config.db_passwd
     );
@@ -218,22 +265,28 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
+//Aaron Comment: looks for applicaiton to be run. If not found, program exits.
     sprintf(buf, "where name='%s'", app_name);
     if (app.lookup(buf)) {
         log_messages.printf(MSG_CRITICAL, "can't find app %s\n", app_name);
         exit(1);
     }
 
+//Aaron Comment: looks for work templates, if cannot find, or are corrupted,
+//the program exits.
     sprintf(buf, "templates/%s", in_template_file);
     if (read_file_malloc(config.project_path(buf), in_template)) {
         log_messages.printf(MSG_CRITICAL, "can't read input template %s\n", buf);
         exit(1);
     }
 
+//Aaron Comment: if work generator passes all startup tests, the main work gneration
+//loop is called.
     start_time = time(0);
     seqno = 0;
 
     log_messages.printf(MSG_NORMAL, "Starting\n");
 
-    main_loop();
+    //TODO: Parse M and N from command line
+    make_jobs(max_set_value, set_size);
 }
