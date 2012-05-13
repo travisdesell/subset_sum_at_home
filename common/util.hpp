6b4037e2c215b2bcd454381030d70cd25bd516bf
Travis Desell <travis.desell@gmail.com>
2012-05-13 18:04:01 -0500

    Added new style validator.

--- common/util.hpp
+++ common/util.hpp
@@ -3,26 +3,13 @@
 
 #include <iostream>
 #include <string>
-#include <stdint.h>
+#include <vector>
+
+#include "stdint.h"
 
 using namespace std;
 
 const char *FGDO_UTIL_ERRORS[] = { "success", "XML tag not found" };
-
-string convert_string(const char* cstr) {
-    string str(cstr);
-    return str;
-}
-
-uint32_t convert_unsigned_int(const char* cstr) {
-    stringstream str_value;
-    str_value << cstr;
-
-    uint32_t int_value;
-    str_value >> int_value;
-
-    return int_value;
-}
 
 template <class T>
 vector<T>* string_to_vector(string s, T (*convert)(const char*) ) {
@@ -55,7 +42,7 @@ string vector_to_string(vector<T> *v) {
 }
 
 template <class T>
-T parse_xml(string xml, string tag, T (*convert)(const char*) ) throw (string) {
+T parse_xml(string xml, const char tag[]) throw (string) {
     string start_tag("<");
     start_tag.append(tag);
     start_tag.append(">");
@@ -71,7 +58,11 @@ T parse_xml(string xml, string tag, T (*convert)(const char*) ) throw (string) {
     if (length > 0) {
 //        cout << "parsing: " << tag << " from '" << xml.substr(start, length) << "'" << endl;
 
-        return convert( xml.substr(start, length).c_str() );
+        istringstream data( xml.substr(start, length) );
+        T value;
+        data >> value;
+
+        return value;
     } else {
         ostringstream oss;
         oss << "Tag '" << tag << "' was not found, find(" << start_tag << ") returned " << start << ", find(" << end_tag << ") returned " << end << ", error in file [" << __FILE__ << "] on line [" << __LINE__ << "]" << endl;
@@ -80,7 +71,7 @@ T parse_xml(string xml, string tag, T (*convert)(const char*) ) throw (string) {
 }
 
 template <>
-string parse_xml<string>(string xml, string tag, string (*convert)(const char*) ) throw (string) {
+string parse_xml<string>(string xml, const char tag[]) throw (string) {
     string start_tag("<");
     start_tag.append(tag);
     start_tag.append(">");
@@ -96,7 +87,7 @@ string parse_xml<string>(string xml, string tag, string (*convert)(const char*) 
     if (length > 0) {
 //        cout << "parsing: " << tag << " from '" << xml.substr(start, length) << "'" << endl;
 
-        return convert( xml.substr(start, length).c_str() );
+        return xml.substr(start, length);
     } else if (length == 0) {
         return string();
     } else {
@@ -106,5 +97,38 @@ string parse_xml<string>(string xml, string tag, string (*convert)(const char*) 
     }
 }
 
+template <class T>
+void parse_xml_vector(string xml, const char tag[], vector<T> &result) throw (string) {
+    string start_tag("<");
+    start_tag.append(tag);
+    start_tag.append(">");
+
+    string end_tag("</");
+    end_tag.append(tag);
+    end_tag.append(">");
+
+    int start = xml.find(start_tag, 0) + start_tag.size();
+    int end = xml.find(end_tag, 0);
+    int length = end - start;
+
+    if (length > 0) {
+        cout << "parsing: " << tag << " from '" << xml.substr(start, length) << "'" << endl;
+
+        istringstream data( xml.substr(start, length) );
+        T value;
+        while (data >> value) {
+            cout << "    " << value << endl;
+            result.push_back(value);
+        }
+
+        return;
+    } else if (length == 0) {
+        return;
+    } else {
+        ostringstream oss;
+        oss << "Tag '" << tag << "' was not found, find(" << start_tag << ") returned " << start << ", find(" << end_tag << ") returned " << end << ", error in file [" << __FILE__ << "] on line [" << __LINE__ << "]" << endl;
+        throw oss.str();
+    }
+}
 
 #endif
