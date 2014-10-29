4ec24f392b5479aac721fbd7f9fb38cdbb697301
Travis Desell <travis.desell@gmail.com>
2014-10-28 21:03:09 -0500

    Fixed links to images

--- webpage/progress.php
+++ webpage/progress.php
@@ -6,10 +6,8 @@ $cwd[__FILE__] = dirname($cwd[__FILE__]);
 
 require_once($cwd[__FILE__] . "/../../citizen_science_grid/header.php");
 require_once($cwd[__FILE__] . "/../../citizen_science_grid/navbar.php");
-require_once($cwd[__FILE__] . "/../../citizen_science_grid/news.php");
 require_once($cwd[__FILE__] . "/../../citizen_science_grid/footer.php");
 require_once($cwd[__FILE__] . "/../../citizen_science_grid/my_query.php");
-require_once($cwd[__FILE__] . "/../../citizen_science_grid/uotd.php");
 
 print_header("SubsetSum@Home: Progress", "", "subset_sum");
 print_navbar("Projects: SubsetSum@Home", "SubsetSum@Home");
