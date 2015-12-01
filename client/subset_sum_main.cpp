8eb99d32d9ae15d353ae5b1e0585d3354ba0bc5a
Dayln Limesand <dayln@Daylns-MacBook-Pro.local>
2015-11-30 20:10:57 -0600

    Created the opencl header with basic boiler plating

--- client/subset_sum_main.cpp
+++ client/subset_sum_main.cpp
@@ -33,7 +33,7 @@
 /**
  *  Includes for subset sum
  */
-
+#include "../demo/opencl_bit_logic.hpp"
 #include "../common/bit_logic.hpp"
 #include "../common/generate_subsets.hpp"
 #include "../common/binary_output.hpp"
@@ -67,7 +67,7 @@ static inline bool test_subset(const uint32_t *subset, const uint32_t subset_siz
     uint32_t max_subset_sum = 0;
 
     for (uint32_t i = 0; i < subset_size; i++) max_subset_sum += subset[i];
-    
+
     for (uint32_t i = 0; i < max_sums_length; i++) {
         sums[i] = 0;
         new_sums[i] = 0;
@@ -78,10 +78,15 @@ static inline bool test_subset(const uint32_t *subset, const uint32_t subset_siz
     for (uint32_t i = 0; i < subset_size; i++) {
         current = subset[i];
 
+        #ifdef _OpenCl_
+        cl_shift_left(new_sums, max_sums_length, subset_size, sums, current);                    // new_sums = sums << current;
+        #else
         shift_left(new_sums, max_sums_length, sums, current);                    // new_sums = sums << current;
 //        *output_target << "new_sums = sums << %2u    = ", current);
 //        print_bit_array(new_sums, sums_length);
 //        *output_target << "\n");
+        #endif
+
 
         or_equal(sums, max_sums_length, new_sums);                               //sums |= new_sums;
 //        *output_target << "sums |= new_sums         = ");
@@ -106,7 +111,7 @@ static inline bool test_subset(const uint32_t *subset, const uint32_t subset_siz
             checksum += sums[i];
         } else { // avoid the overflow
             checksum = sums[i] - (UINT32_MAX - checksum);
-        } 
+        }
     }
 
 #endif
@@ -120,7 +125,7 @@ void write_checkpoint(string filename, const uint64_t iteration, const uint64_t 
     if (retval) {
         cerr << "APP: error writing checkpoint (resolving checkpoint file name)" << endl;
         return;
-    }   
+    }
 
     ofstream checkpoint_file(output_path.c_str());
 #else
@@ -129,7 +134,7 @@ void write_checkpoint(string filename, const uint64_t iteration, const uint64_t 
     if (!checkpoint_file.is_open()) {
         cerr << "APP: error writing checkpoint (opening checkpoint file)" << endl;
         return;
-    }   
+    }
 
     checkpoint_file << "iteration: " << iteration << endl;
     checkpoint_file << "pass: " << pass << endl;
@@ -239,7 +244,7 @@ T parse_t(const char* arg) {
         else if (n[i] == '4') val = 4;
         else if (n[i] == '5') val = 5;
         else if (n[i] == '6') val = 6;
-        else if (n[i] == '7') val = 7; 
+        else if (n[i] == '7') val = 7;
         else if (n[i] == '8') val = 8;
         else if (n[i] == '9') val = 9;
         else {
@@ -249,7 +254,7 @@ T parse_t(const char* arg) {
 #endif
             exit(1);
         }
-        
+
         result += place * val;
         place *= 10;
     }
@@ -413,7 +418,7 @@ int main(int argc, char** argv) {
     uint32_t *subset = new uint32_t[subset_size];
 
 //    this caused a problem:
-//    
+//
 //    *output_target << "%15u ", 296010);
 //    generate_ith_subset(296010, subset, subset_size, max_set_value);
 //    print_subset(subset, subset_size);
@@ -645,4 +650,3 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR Args, int WinMode
     return main(argc, argv);
 }
 #endif
-
